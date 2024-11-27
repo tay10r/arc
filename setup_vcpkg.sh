be9e4053ae47cdb097271d34322b55a2ff3f4e58
Taylor Holberton <taylorcholberton@gmail.com>
2024-11-27 13:56:21 -0500

    Add protobuf to dependency list

--- setup_vcpkg.sh
+++ setup_vcpkg.sh
@@ -6,6 +6,6 @@ if [ ! -e vcpkg ]; then
   export VCPKG_ROOT="${PWD}"
   git checkout "2024.11.16"
   ./bootstrap-vcpkg.sh -disableMetrics
-  ./vcpkg install cxxopts spdlog libuv gtest glfw3 glm
+  ./vcpkg install cxxopts spdlog libuv gtest glfw3 glm protobuf
   popd
 fi
